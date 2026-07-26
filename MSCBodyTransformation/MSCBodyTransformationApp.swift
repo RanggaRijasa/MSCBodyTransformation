9e2d7795cd0c1be68595297078171562fce5956f
RanggaRijasa <ranggarijasa2005@gmail.com>
2026-07-26 14:45:33 +0700

    Initial Commit

---
+++ MSCBodyTransformation/MSCBodyTransformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSCBodyTransformationApp.swift
+//  MSCBodyTransformation
+//
+//  Created by Rangga Rijasa on 26/07/26.
+//
+
+import SwiftUI
+
+@main
+struct MSCBodyTransformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MSCBodyTransformationTests/MSCBodyTransformationTests.swift
@@ -0,0 +1,19 @@
+//
+//  MSCBodyTransformationTests.swift
+//  MSCBodyTransformationTests
+//
+//  Created by Rangga Rijasa on 26/07/26.
+//
+
+import Testing
+@testable import MSCBodyTransformation
+
+struct MSCBodyTransformationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
